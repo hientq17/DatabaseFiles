6d26f1225b97eea3eba8352664bf82ac9b075c2e
Trinh Quang Hien <quanghien1712@gmail.com>
2020-11-12 16:38:16 +0700

    Initial commit

---
+++ BasicProgram.sql
@@ -0,0 +1,89 @@
+﻿/*Basic Programming in SQL
+Declare Variable in SQL
+Granted Value
+Control Structures in SQL (If, case, while)
+Block of code 
+------------------
+Declare @Varname data type [=initial value]
+Granted value: set, select
+*/
+Declare @name varchar(50), @age int=30, @gender bit
+set @name='John'
+Select @gender=1
+print 'Hello' +@name
+if @gender=1 print 'Hello Mr' +@name
+else print 'Hello Ms'+@name
+--while loop
+declare @i int=0, @n int=10, @result int=0
+while (@i <= @n)
+begin
+	set @result = @result+@i
+	set @i += 1
+end
+print str(@result)
+declare @j int = 0
+while (@j < 10)
+begin
+	if(@j<7) print 'Hoc database'
+	else print 'Da nho'
+	set @j += 1
+end
+--Kiem tra so nguyen to
+declare @so int = 2, @k int = 2, @tmp bit=1
+while(@k <= sqrt(@so))
+begin
+	if(@so % @k = 0) 
+		begin 
+			set @tmp = 0 
+			break 
+		end
+	set @k += 1  
+end
+ if @tmp = 1 print str(@so)+ ' la so nguyen to'
+		else print str(@so)+ ' khong phai la so nguyen to'
+declare @kwh int =100, @pay decimal(10,2)
+set  @pay = case 
+	when @kwh between 0 and 50 then @kwh*1678
+	when @kwh between 51 and 100 then  50*1678 + (@kwh-50)*1734
+	when @kwh between 101 and 200 then 50*1678 + 100*1734 + (@kwh-100)*2014
+	when @kwh between 201 and 300 then 50*1678 + 100*1734 + 200*2014 + (@kwh-200)*2536
+	when @kwh between 301 and 400 then 50*1678 + 100*1734 + 200*2014 + 300*2536 + (@kwh-300)*2834
+	when @kwh >= 401 then 50*1678 + 100*1734 + 200*2014 + 300*2536 + 400*2834 + (@kwh-400)*2461
+end
+print str(@pay*1.1)
+--User variable begin with @
+--System variable begin with @@ (ex @@erro)
+use JOB_DB
+Delete from JOBs where JobNo=5 --xoá dữ liệu trong bảng
+Truncate table Jobs --xoá bảng
+Delete Jobs --xoá bảng (giống với truncate)
+Use bank
+Select * from Accounts
+ --Rollback Case
+Begin Transaction T1
+Update Accounts
+set Balance = 0
+where AccNo = '0001'
+Rollback Transaction T1 --Nothing change on 0001
+ --Commit case
+Begin Transaction T2
+Update Accounts
+set Balance = 0
+where AccNo = '0001'
+Commit Transaction T2 -- 0001 has been changed
+---------
+Begin Transaction T3
+Update Accounts set Balance = 100 where AccNo = '0001'
+Save transaction SP1
+Update Accounts set Balance = 200 where AccNo = '0002'
+Save transaction SP2
+Update Accounts set Balance = 300 where AccNo = '0003'
+Rollback transaction SP1
+Update Accounts set Balance = 400 where AccNo = '0004'
+Commit Transaction T3 --Nothing changes on 0002 & 0003
+--Điều khiển tương tranh
+Begin Transaction 
+update accounts set balance = 1100 where accno='0001'
+waitfor delay '00:00:10'
+Rollback transaction 
+
---
+++ JOB.sql
@@ -0,0 +1,36 @@
+﻿use JOB_DB
+Select* from EMPS
+Select* from JOBS
+Select distinct Skill from emps 
+Select top 3* from EMPS order by SALARY DESC
+Select * from emps where salary>1000
+Select Skill, count(*), sum(salary), max(salary), min(salary), avg(salary) as Skill_count from emps Group by skill
+Select skill, count(*) as Skill_count from Emps Group by Skill having count(*)>2
+Select * from Emps, Jobs /*Cross join*/
+Select * from Emps inner join Jobs on Emps.Skill=Jobs.skill /*inner join*/
+Select * from Emps Left join Jobs on Emps.Skill=Jobs.skill /*left join*/
+Select * from Emps Right join Jobs on Emps.Skill=Jobs.skill /*right join*/
+Select * from Emps Full join Jobs on Emps.Skill=Jobs.skill /*full join*/
+Select Emps.* from Emps where Skill not in (Select Skill from Jobs where JobNo=1) 
+Select * from Emps where skill in('SQL','Java','C#')
+Select * from Emps where Salary between 1200 and 1300
+Select * from Emps where DateOfBirth between '1980-03-01' and '1980-03-20'
+Select* from Emps where Skill like 'C%' --lấy bất kì giá trị nào bắt đầu từ C
+Select* from Emps where Skill like 'C_' --lấy kí tự bắt đầu từ C và theo sau nó là 1 kí tự
+Select* from Emps where Skill like 'C__' --lấy kí tự bắt đầu từ C và theo sau nó là 2 kí tự
+Select* from Emps where Salary like '12__'
+Select skill,count(*) as Skill_count from emps group by skill union select 'Number of Employees', count(*) as Emps_Count from Emps order by count(*) ASC
+--Cho biết thông tin các nhân viên có kĩ năng không trùng với bất kì người nào ở trong công ty
+Select emps.* from EMps where skill in(select skill from emps group by skill having count(*)=1 )
+--Cho biết thông tin các nhân viên có ngày sinh nhật trùng với ít nhất 1 người trong công ty
+Select* from Emps except 
+select T1.* from Emps as T1, Emps as T2 
+	where day(T1.DateOfBirth) = day(T2.DateOfBirth)
+	and month(T1.DateOfBirth) = month(T2.DateOfBirth)
+	and T1.ENO<>T2.ENO
+--Tăng lương 10% cho 2 nhân viên có mức lương thấp nhất
+Select * from EMps 
+	Update emps set EMps.SALARY=T.salary*1.1
+	from (select top 2* from emps order by salary asc) as T
+	where emps.eno = T.eno
+--Viết truy vấn tính tổng lương của từng skill và đưa ra số lượng đồng thời cũng xuất ra lương của từng nhân viên --
